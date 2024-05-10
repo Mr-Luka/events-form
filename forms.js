d82c302c2408888f9816b4509e8ab0b11db533bc
Luka <luka.luka.luka93@gmail.com>
2024-05-10 15:30:51 -0700

    e.preventdefault

--- forms.js
+++ forms.js
@@ -1,5 +1,10 @@
 const wes = document.querySelector(".wes");
 
 wes.addEventListener("click", function (e){
-    console.log(e)
+    console.log("You clicked it!");
+    const shouldChangePage = confirm("this website might be malicious! do you wish to proceed?");
+    if(!shouldChangePage) {
+        e.preventDefault()
+    }
+    console.log(shouldChangePage)
 })
