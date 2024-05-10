32698416c7e30daeaa020cf8c48e26b4bc5b4ba2
Luka <luka.luka.luka93@gmail.com>
2024-05-10 15:33:29 -0700

    form submit event listener

--- forms.js
+++ forms.js
@@ -1,4 +1,6 @@
 const wes = document.querySelector(".wes");
+const form = document.querySelector(`[name="signup"]`);
+
 
 wes.addEventListener("click", function (e){
     console.log("You clicked it!");
@@ -7,4 +9,9 @@ wes.addEventListener("click", function (e){
         e.preventDefault()
     }
     console.log(shouldChangePage)
+})
+
+form.addEventListener("submit", function(e){
+    console.log(e)
+    e.preventDefault()
 })
